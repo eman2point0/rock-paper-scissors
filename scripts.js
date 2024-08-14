6a47bc8cd0083c235b77e3933b2e4b420a49c3b9
eman2point0 <eman2point0@yahoo.com>
2024-08-13 20:34:55 -0400

    UI Changes

--- scripts.js
+++ scripts.js
@@ -27,9 +27,10 @@ function getHumanChoice() {
   return choice;
 }
 
-function playRound() {
+function playRound(humanPick) {
   let computer = getComputerChoice();
-  let human = getHumanChoice();
+  const human = humanPick;
+  displayChoice(human, computer);
   if (human.localeCompare(computer) === 0) {
     console.log("Tie");
   } else if (human === "Rock" && computer === "Scissors") {
@@ -46,15 +47,61 @@ function playRound() {
     computerScore++;
   }
 
-  console.log(
-    "Score is now: Computer " + computerScore + " Human " + humanScore
-  );
-}
-
-function playGame() {
-  for (let i = 0; i < 5; i++) {
-    playRound();
+  updateScore();
+  if (humanScore === 5 || computerScore === 5) {
+    humanScore === 5 ? revealWinner("You ") : revealWinner("Computer");
   }
 }
 
-playGame();
+/* DOM Manipulation */
+
+const body = document.body;
+
+/* Let's construct the play button */
+const playBtn = document.querySelector("#playButton");
+playBtn.onclick = () => {
+  revealOptions();
+};
+
+/*Make it so that when the button is click, the 3 options show up */
+
+function revealOptions() {
+  playBtn.style.display = "none";
+  const optContainer = document.querySelector(".options");
+  optContainer.style.display = "flex";
+}
+
+/* Button interactivity */
+const rockBtn = document.querySelector("#rockBtn");
+rockBtn.onclick = () => playRound("Rock");
+
+const paperBtn = document.querySelector("#paperBtn");
+paperBtn.onclick = () => playRound("Paper");
+
+const scissorsBtn = document.querySelector("#scissorsBtn");
+scissorsBtn.onclick = () => playRound("Scissors");
+
+/* Show what option player/computer chooses*/
+const choiceNode = document.querySelectorAll(".choice");
+
+function displayChoice(humanChoice, cptrChoice) {
+  choiceNode[0].textContent = `${humanChoice}`;
+  choiceNode[1].textContent = `${cptrChoice}`;
+}
+
+console.log(choiceNode);
+
+/*Change score when player/computer wins*/
+
+function updateScore() {
+  const playerScore = document.querySelector(".player .score");
+  playerScore.textContent = `${humanScore}`;
+  const cptr = document.querySelector(".computer .score");
+  cptr.textContent = `${computerScore}`;
+}
+
+/* Reveal the winner when player/computer reaches 5 wins */
+function revealWinner(winner) {
+  const winnerDiv = document.querySelector(".winAnnouncement");
+  winnerDiv.textContent = `${winner} WIN with 5 points!`;
+}
